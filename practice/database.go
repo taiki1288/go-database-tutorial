090e0e437b3d146fbf31056dc28d5ae296917b8e
taiki yamashita <taikity0808@gmail.com>
2022-01-01 22:46:38 +0900

    database.go

--- practice/database.go
+++ practice/database.go
@@ -11,3 +11,27 @@ var (
 	id   int
 	name string
 )
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err := db.Query("SELECT id, name FROM users WHERE id = ?", 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
